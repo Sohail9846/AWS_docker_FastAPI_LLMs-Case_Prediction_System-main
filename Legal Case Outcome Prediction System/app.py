3b2326bf31b1e222b2a06ee39bffec9b1e727254
Sohail9846 <sohailansari9846@gmail.com>
2026-01-02 18:35:46 +0530

    Update app.py

--- Legal Case Outcome Prediction System/app.py
+++ Legal Case Outcome Prediction System/app.py
@@ -4,7 +4,8 @@ my_image = modal.Image.from_dockerfile(".")
 
 app = modal.App("fastapi-ml-app")
 
-@app.function(gpu="A10G", image=my_image)  # GPU support here
+@app.function(gpu="A10G", image=my_image)
 def run_my_app():
     import subprocess
     subprocess.run(["uvicorn", "inference:app", "--host", "0.0.0.0", "--port", "80"])
+
